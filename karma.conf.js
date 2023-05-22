0a0238ea977f619aa7a9bdd041153d36ded14d55
Izza wildan ridhoni <izzawildan88@gmail.com>
2023-05-22 16:26:45 +0700

    Add dotenv-webpack and reconfigure webpack and karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,25 @@
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
     /* Kode lainnya disembunyikan */
- 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
- 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
- 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
- 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
+    webpack: {
+      devtool: 'inline-source-map',
+      mode: 'development',
+      plugins: [
+        new Dotenv({
+          path: path.resolve(__dirname, '.env'),
+          systemvars: true,
+          safe: true,
+        }),
+      ],
+    },
+
+    webpackMiddleware: {
+      stats: 'errors-only',
+    },
+
+    /* Kode lainnya disembunyikan */
   });
 };
